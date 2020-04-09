f492f4cc762f51e9b8a8b689aae0a9539437d83e
Samuel Fry <samuel.fry13@gmail.com>
2020-04-09 10:13:10 -0600

    fix proxy for self_signed_cert

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,6 +3,7 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(createProxyMiddleware('/api/', {
     changeOrigin: true,
+    secure: false,
     pathRewrite: {'^/api': ''}, // remove leading /api to match real API urls
     target: (process.env.REACT_APP_BACKEND || 'http://localhost:8080'),
   }));
